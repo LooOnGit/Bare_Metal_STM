d7117db30fd826f07b7c36187616d923fe592ddf
LooOnGit <tranconghoa24@gmail.com>
2025-12-30 05:37:27 +0700

    Keep Header_CMSIS/STM32CubeF4 directory

--- Inc/gpio.h
+++ Inc/gpio.h
@@ -2,4 +2,15 @@
 #define __GPIO__C__
 #include <stm32f411xe.h>
 
-#enm
+#endif // __GPIO__H__
+
+:: tạo .gitkeep
+type NUL > Header_CMSIS\STM32CubeF4\.gitkeep
+
+:: stage .gitkeep (và nếu muốn, stage thay đổi gpio.h)
+git add Header_CMSIS\STM32CubeF4\.gitkeep
+git add Inc\gpio.h
+
+:: commit và push
+git commit -m "Keep Header_CMSIS/STM32CubeF4 directory"
+git push
