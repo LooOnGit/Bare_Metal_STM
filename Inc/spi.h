adf7a3da75fafb2a2197813f56859cbae8fdcc4a
LooOnGit <tranconghoa24@gmail.com>
2026-01-13 06:19:12 +0700

    add description SPI

--- Inc/spi.h
+++ Inc/spi.h
@@ -1,14 +1,117 @@
+/**
+ * @file spi.h
+ * @brief SPI (Serial Peripheral Interface) driver header file for STM32F411
+ * 
+ * This module provides functions for configuring and using the SPI1 peripheral
+ * on the STM32F411 microcontroller. It handles initialization of GPIO pins,
+ * SPI configuration, data transmission/reception, and chip select control.
+ * 
+ * @details
+ * The driver uses the following pins:
+ * - PA5: SCK (Serial Clock)
+ * - PA6: MISO (Master In Slave Out)
+ * - PA7: MOSI (Master Out Slave In)
+ * - PA9: CS (Chip Select)
+ * 
+ * The SPI is configured in Master mode with:
+ * - Clock polarity (CPOL) = 1
+ * - Clock phase (CPHA) = 1
+ * - 8-bit data format
+ * - MSB first
+ * - Full-duplex mode
+ * - Clock frequency = fPCLK/4
+ * 
+ * @author Bare Metal STM32
+ * @version 1.0
+ * @date 2026
+ */
+
 #ifndef __SPI_H__
 #define __SPI_H__
 
 #define STM32F411xE
 #include "stm32f4xx.h"
 
+/**
+ * @brief Initialize SPI1 GPIO pins
+ * 
+ * Configures PA5, PA6, PA7, and PA9 as alternate functions for SPI1 operation.
+ * - PA5: SCK (Serial Clock)
+ * - PA6: MISO (Master In Slave Out)
+ * - PA7: MOSI (Master Out Slave In)
+ * - PA9: CS (Chip Select) - General Purpose Output
+ * 
+ * @return void
+ */
 void spiInit(void);
+
+/**
+ * @brief Configure and enable SPI1 peripheral
+ * 
+ * Sets up the SPI1 control registers with the following configuration:
+ * - Master mode enabled
+ * - Clock prescaler = fPCLK/4
+ * - Clock polarity (CPOL) = 1 (CK to 1 when idle)
+ * - Clock phase (CPHA) = 1 (Data captured on 2nd edge)
+ * - Full-duplex mode
+ * - 8-bit data frame format
+ * - MSB transmitted first
+ * - Software slave management enabled
+ * - SPI module enabled
+ * 
+ * @note Call spiInit() before calling this function
+ * @return void
+ */
 void spi1Config(void);
+
+/**
+ * @brief Transmit data via SPI1
+ * 
+ * Sends data over the SPI bus. This function blocks until all data is transmitted
+ * and the SPI bus becomes idle. The slave must be selected before calling this function.
+ * 
+ * @param[in] data Pointer to the data buffer containing the bytes to transmit
+ * @param[in] size Number of bytes to transmit
+ * 
+ * @note The function waits for TXE (Transmit buffer empty) flag before each transmission
+ *       and waits for BSY (Busy) flag to clear before returning
+ * @return void
+ */
 void spi1Transmit(uint8_t *data, uint32_t size);
+
+/**
+ * @brief Receive data via SPI1
+ * 
+ * Receives data from the SPI bus by transmitting dummy data (0x00) to generate
+ * the SPI clock. This function blocks until all requested data is received.
+ * 
+ * @param[out] data Pointer to the buffer where received data will be stored
+ * @param[in] size Number of bytes to receive
+ * 
+ * @note The slave must be selected before calling this function
+ *       Dummy data (0x00) is transmitted to generate the clock
+ * @return void
+ */
 void spi1Receive(uint8_t *data, uint32_t size);
+
+/**
+ * @brief Enable chip select (pull CS low)
+ * 
+ * Sets PA9 to low logic level, selecting the slave device.
+ * This function must be called before any SPI transmission or reception.
+ * 
+ * @return void
+ */
 void csEnable(void);
+
+/**
+ * @brief Disable chip select (pull CS high)
+ * 
+ * Sets PA9 to high logic level, deselecting the slave device.
+ * This function must be called after SPI transmission or reception is complete.
+ * 
+ * @return void
+ */
 void csDisable(void);
 
 
--- Src/spi.c
+++ Src/spi.c
@@ -1,5 +1,33 @@
+/**
+ * @file spi.c
+ * @brief SPI (Serial Peripheral Interface) driver implementation for STM32F411
+ * 
+ * This file contains the implementation of SPI1 initialization, configuration,
+ * and data transmission/reception functions for the STM32F411 microcontroller.
+ * 
+ * @author Bare Metal STM32
+ * @version 1.0
+ * @date 2026
+ */
+
 #include "spi.h"
 
+/**
+ * @brief Initialize SPI1 GPIO pins
+ * 
+ * @details
+ * Enables clock access to GPIOA and configures the following pins:
+ * - PA5: Alternate Function (SCK - Serial Clock)
+ * - PA6: Alternate Function (MISO - Master In Slave Out)
+ * - PA7: Alternate Function (MOSI - Master Out Slave In)
+ * - PA9: General Purpose Output (CS - Chip Select)
+ * 
+ * All pins are configured with their respective alternate function types
+ * for SPI1 operation.
+ * 
+ * @return void
+ * @see spi1Config()
+ */
 void spiInit(void)
 {
     /*Enable clock access to GPIOA*/
@@ -43,6 +71,26 @@ void spiInit(void)
     GPIOA->AFR[3] &= ~(1U<<31);
 }
 
+/**
+ * @brief Configure and enable SPI1 peripheral
+ * 
+ * @details
+ * Performs the following configuration:
+ * - Enables the SPI1 clock via RCC APB2
+ * - Sets the clock prescaler to fPCLK/4
+ * - Configures clock polarity (CPOL = 1) - CK remains 1 when idle
+ * - Configures clock phase (CPHA = 1) - Data captured on second edge
+ * - Enables full-duplex mode
+ * - Sets MSB first bit order
+ * - Configures as Master mode
+ * - Sets 8-bit data frame format
+ * - Enables software slave management (SSM = 1, SSI = 1)
+ * - Enables the SPI module
+ * 
+ * @note spiInit() must be called before this function to configure GPIO pins
+ * @return void
+ * @see spiInit()
+ */
 void spi1Config(void)
 {
     /*Enable clock access to SPI1*/
@@ -77,6 +125,27 @@ void spi1Config(void)
     SPI1->CR1 |= (1U<<6);
 }
 
+/**
+ * @brief Transmit data via SPI1
+ * 
+ * @details
+ * Sends data through the SPI bus in blocking mode. The function:
+ * 1. Waits for the Transmit Buffer Empty (TXE) flag for each byte
+ * 2. Writes the byte to the SPI Data Register (DR)
+ * 3. After all data is transmitted, waits for TXE to be set
+ * 4. Waits for the Busy (BSY) flag to clear
+ * 5. Clears any potential overflow flag by dummy reading DR and SR
+ * 
+ * @param[in] data Pointer to the buffer containing data to transmit
+ * @param[in] size Number of bytes to transmit
+ * 
+ * @note - Blocking function; does not return until transmission is complete
+ *       - Slave must be selected using csEnable() before calling this function
+ *       - Function blocks until BSY flag is cleared
+ * 
+ * @return void
+ * @see csEnable(), spi1Receive()
+ */
 void spi1Transmit(uint8_t *data, uint32_t size)
 {
     uint32_t i = 0;
@@ -102,6 +171,27 @@ void spi1Transmit(uint8_t *data, uint32_t size)
     temp = SPI1->SR;
 }
 
+/**
+ * @brief Receive data via SPI1
+ * 
+ * @details
+ * Receives data from the SPI bus by transmitting dummy data (0x00) to generate
+ * the required clock signals. The function:
+ * 1. Writes 0x00 to the SPI Data Register to start SPI clock
+ * 2. Waits for the Receive Buffer Not Empty (RXNE) flag
+ * 3. Reads received data from the Data Register
+ * 4. Repeats until all requested bytes are received
+ * 
+ * @param[out] data Pointer to the buffer where received data will be stored
+ * @param[in] size Number of bytes to receive
+ * 
+ * @note - Blocking function; does not return until all data is received
+ *       - Slave must be selected using csEnable() before calling this function
+ *       - Dummy bytes (0x00) are transmitted to generate the clock
+ * 
+ * @return void
+ * @see csEnable(), spi1Transmit()
+ */
 void spi1Receive(uint8_t *data, uint32_t size)
 {
     while(size)
@@ -116,11 +206,33 @@ void spi1Receive(uint8_t *data, uint32_t size)
     }
 }
 
+/**
+ * @brief Enable chip select
+ * 
+ * @details
+ * Pulls PA9 (Chip Select) to low logic level, selecting the slave device
+ * for SPI communication. This function must be called before any SPI
+ * transmission or reception operation.
+ * 
+ * @return void
+ * @see csDisable(), spi1Transmit(), spi1Receive()
+ */
 void csEnable(void)
 {
     GPIOA->ODR &=~ (1U<<9);
 }
 
+/**
+ * @brief Disable chip select
+ * 
+ * @details
+ * Pulls PA9 (Chip Select) to high logic level, deselecting the slave device.
+ * This function must be called after SPI transmission or reception operations
+ * are complete to signal the end of communication.
+ * 
+ * @return void
+ * @see csEnable(), spi1Transmit(), spi1Receive()
+ */
 void csDisable(void)
 {
     GPIOA->ODR |= (1U<<9);
